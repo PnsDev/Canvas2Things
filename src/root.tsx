08dce7dbc5db6424a11d401005a57540a3b4c7b0
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2023-05-07 16:18:21 -0500

    Added decorations to website previews

--- src/root.tsx
+++ src/root.tsx
@@ -15,6 +15,24 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
+        <title>Canvas2Things</title>
+        <meta name="description" content="Upgrade your scheduling experience with seamless Canvas Calendar integration for Things 3" />
+        
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        <meta property="og:url" content="https://canvas2things.pns.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Canvas2Things" />
+        <meta property="og:description" content="Upgrade your scheduling experience with seamless Canvas Calendar integration for Things 3" />
+        <meta property="og:image" content="/preview.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="canvas2things.pns.dev" />
+        <meta property="twitter:url" content="/preview.png" />
+        <meta name="twitter:title" content="Canvas2Things" />
+        <meta name="twitter:description" content="Upgrade your scheduling experience with seamless Canvas Calendar integration for Things 3" />
+        <meta name="twitter:image" content="/preview.png" />
+
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
